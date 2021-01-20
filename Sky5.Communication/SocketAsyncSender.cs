02c37e45c679b05a94ea18ec8dec36e190d01284
MingTao <3073429849@qq.com>
2021-01-20 11:11:14 +0800

    用Interlocked构造了生产者消费者模式，有错误

--- Sky5.Communication/SocketAsyncSender.cs
+++ Sky5.Communication/SocketAsyncSender.cs
@@ -13,8 +13,17 @@ namespace Sky5.Communication
     public class SocketAsyncSender: IBufferWriter<byte>
     {
         public readonly Socket Socket;
+        volatile SendAble EmptyNode = new EmptySendAble();
         volatile SendAble First;
         volatile SendAble Last;
+        class EmptySendAble : SendAble
+        {
+            public override void SetBuffer(SocketAsyncSender sender, byte[] buffer, ref int offset, ref bool flush, out bool completed)
+            {
+                flush = true;
+                completed = true;
+            }
+        }
         object QueueLockObject => eventArgs.Create;
         public Encoding Encoding = Encoding.UTF8;
         SocketAsyncEventArgsWeakReference eventArgs;
@@ -38,53 +47,79 @@ namespace Sky5.Communication
         public void Enqueue(SendAble s)
         {
             s.Next = null;
-            lock (QueueLockObject)
+            while (true)
             {
-                if (First == null)
+                var last = Interlocked.CompareExchange(ref Last, s, null);// 只有消费者线程可以设置Last为null
+                if (last == null)
                 {
-                    Last = s;
                     First = s;
+                    while (eventArgs.Value != null)
+                        Thread.Sleep(0);
                     eventArgs.Begin();
-                    Send(eventArgs.Value);
+                    LoopSend(false, s, eventArgs.Value);
+                    break;
                 }
-                else
+
+                if (Interlocked.CompareExchange(ref last.Next, s, null) == null)
                 {
-                    Last.Next = s;
                     Last = s;
+                    break;
                 }
             }
         }
         volatile int offset;
         public bool AutoFlush;
-        void Send(SocketAsyncEventArgs e)// 保证不被并行调用
+        void LoopSend(bool sync, SendAble first, SocketAsyncEventArgs e)// 保证不被并行调用
         {
-        LOOP:
-            var flush = AutoFlush;
-            First.SetBuffer(this, e.Buffer, ref offset, ref flush, out bool completed);
-            if (completed)
-                First = First.Next;
-            if (flush || offset == e.Buffer.Length)
+            while (true)
             {
-                e.SetBuffer(0, offset);
-                offset = 0;
-                if (!Socket.SendAsync(e))
-                    OnCompleted(Socket, e);
+                var flush = AutoFlush;
+                first.SetBuffer(this, e.Buffer, ref offset, ref flush, out bool completed);
+                if (completed)
+                {
+                    First = first = first.Next;
+                    if (first == null) flush = true;
+                }
+                if (flush || offset == e.Buffer.Length)
+                {
+                    e.SetBuffer(0, offset);
+                    offset = 0;
+                    if (!Socket.SendAsync(e))
+                    {
+                        if (sync)
+                            OnCompleted(Socket, e);
+                        else
+                            ThreadPool.QueueUserWorkItem(state => OnCompleted(Socket, e));
+                    }
+                    else
+                    { }
+                    break;
+                }
             }
-            else if(First != null)
-                goto LOOP;
+
         }
 
         void OnCompleted(object sender, SocketAsyncEventArgs e)
         {
-            lock (QueueLockObject)
+            var first = First;
+            if (first == null)
             {
-                if (First == null)
+                while (true)
                 {
-                    Last = null;
-                    eventArgs.Free();
+                    var last = this.Last;
+                    first = First;
+                    if (first == null)
+                    {
+                        if (Interlocked.CompareExchange(ref this.Last, null, last) == last)
+                        {
+                            eventArgs.Free();
+                            return;
+                        }
+                    }
+                    else break;
                 }
-                else Send(e);
             }
+            LoopSend(true, first, e);
         }
         #region IBufferWriter
         byte[] buffer;
